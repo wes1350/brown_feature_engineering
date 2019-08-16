6652cf44545a7d0a3be00e48c8dc9b76f761f58e
Wesley Runnels <wrunnels@mit.edu>
2019-08-16 10:09:57 -0400

    Removed code in highest level init file

--- feature_engineering/__init__.py
+++ feature_engineering/__init__.py
@@ -1,5 +1,5 @@
-from primitive.primitive_code.dataframe_transform import DataframeTransformPrimitive
-__all__ = ['DataframeTransformPrimitive']
+# from primitive.primitive_code.dataframe_transform import DataframeTransformPrimitive
+# __all__ = ['DataframeTransformPrimitive']
 
 # from pkgutil import extend_path
 # __path__ = extend_path(__path__, __name__)  # type: ignore
